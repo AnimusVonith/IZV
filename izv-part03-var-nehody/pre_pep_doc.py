7887b41a6d4399f25f93c23575623e3bac5706a4
AnimusVonith <jakub.adamciak@gmail.com>
2022-08-26 15:28:55 +0200

    pep-8

--- izv-part03-var-nehody/pre_pep_doc.py
+++ izv-part03-var-nehody/pre_pep_doc.py
@@ -5,9 +5,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 # muzete pridat vlastni knihovny
 
-
 def get_data(df: pd.DataFrame) -> str:
-    alcohol_scope = [1, 3, 6, 7, 8, 9]
+    alcohol_scope = [1,3,6,7,8,9]
     df = df.copy()
     df = df[df["p11"].isin(alcohol_scope)]
     df["p11"].replace([1, 3, 6, 7, 8, 9],
@@ -16,25 +15,24 @@ def get_data(df: pd.DataFrame) -> str:
                        "0.80-1.00", "1.00-1.50",
                        "1.50+"], inplace=True)
 
-    plt.figure(figsize=(8, 6))
+    plt.figure(figsize=(8,6))
     sns.countplot(data=df, x="p11", order=["0.01-0.24",
-                                           "0.24-0.50", "0.50-0.80",
-                                           "0.80-1.00", "1.00-1.50",
-                                           "1.50+"])
+                       "0.24-0.50", "0.50-0.80",
+                       "0.80-1.00", "1.00-1.50",
+                       "1.50+"])
     plt.ylabel("Počet vinníkov")
     plt.xlabel("Podieľ alkoholu v krvi (promile)")
-    plt.title("Rozdelenie podľa promile alkoholu u vinníkov autonehôd",
-              size=15, weight="bold")
-
+    plt.title("Rozdelenie podľa promile alkoholu u vinníkov autonehôd", size=15, weight="bold")
+    
     plt.savefig("fig.jpg")
-    # plt.show()
+    #plt.show()
 
     results = []
     results.append(["promile", "počet"])
     for x in sorted(df["p11"].unique()):
         row = []
         row.append(x)
-        row.append(str(df[df["p11"] == x]["p11"].count()))
+        row.append(str(df[df["p11"]==x]["p11"].count()))
         results.append(row)
     return results
 
@@ -48,3 +46,4 @@ if __name__ == "__main__":
     results = get_data(df)
     for row in results:
         print("%9s\t%5s" % (row[0], row[1]))
+        
--- izv-part03-var-nehody/pre_pep_geo.py
+++ izv-part03-var-nehody/pre_pep_geo.py
@@ -9,13 +9,9 @@ import numpy as np
 
 
 def make_geo(df: pd.DataFrame) -> geopandas.GeoDataFrame:
-    """ Konvertovani dataframe do geopandas.GeoDataFrame
-        se spravnym kodovanim"""
+    """ Konvertovani dataframe do geopandas.GeoDataFrame se spravnym kodovanim"""
     df = df.dropna(subset=["d", "e"])
-    gdf = geopandas.GeoDataFrame(
-        df, geometry=geopandas.points_from_xy(df["d"],
-                                              df["e"]),
-        crs="EPSG:5514")
+    gdf = geopandas.GeoDataFrame(df, geometry=geopandas.points_from_xy(df["d"], df["e"]), crs="EPSG:5514")
     return gdf
 
 
@@ -24,8 +20,7 @@ def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None,
     """ Vykresleni grafu s sesti podgrafy podle lokality nehody
     (dalnice vs prvni trida) pro roky 2018-2020 """
 
-    regions = ['PHA', 'STC', 'JHC', 'PLK', 'ULK', 'HKK',
-               'JHM', 'MSK', 'OLK', 'ZLK', 'VYS', 'PAK', 'LBK', 'KVK']
+    regions = ['PHA','STC','JHC','PLK','ULK','HKK','JHM','MSK','OLK','ZLK','VYS','PAK','LBK','KVK']
     roky = [2018, 2019, 2020]
     typy = ["dialnica", "cesta 1. triedy"]
     region = "JHM"
@@ -38,29 +33,26 @@ def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None,
 
     gdf["p2a"] = gdf["p2a"].astype("datetime64")
     gdf["rok"] = gdf["p2a"].dt.year
-
+    
     gdf["dialnica"] = gdf["p36"] == 0
     gdf["cesta 1. triedy"] = gdf["p36"] == 1
 
-    fig, axs = plt.subplots(2, 3, figsize=(18, 8))
+    fig, axs = plt.subplots(2, 3, figsize=(18,8))
     gdf = gdf[gdf["rok"].isin(roky)]
-
-    colors = [["#FF91AF", "#D3212D", "#A52A2A"],
-              ["#7FFFD4", "#007FFF", "#89CFF0"]]
+    
+    colors=[["#FF91AF", "#D3212D", "#A52A2A"], ["#7FFFD4", "#007FFF", "#89CFF0"]]
     for i, rok in enumerate(roky):
         holder = gdf[gdf["rok"] == rok]
         for j, typ in enumerate(typy):
-            holder[holder[typ]].plot(
-                ax=axs[j, i], color=colors[j][i], marker=".", markersize=5)
-            axs[j, i].title.set_text(f"{rok} {typ} v {region} kraji")
+            holder[holder[typ]].plot(ax=axs[j, i], color=colors[j][i], marker=".", markersize=5)
+            axs[j, i].title.set_text(f"{rok} {typ}")
             axs[j, i].set_aspect("auto")
             axs[j, i].axis("off")
-            ctx.add_basemap(axs[j, i], crs=holder.crs,
-                            source=ctx.providers.Stamen.TonerLite)
+            ctx.add_basemap(axs[j, i], crs=holder.crs, source=ctx.providers.Stamen.TonerLite)
 
     plt.tight_layout()
     plt.subplots_adjust(wspace=0.029, right=0.955)
-
+    
     if fig_location is not None:
         plt.savefig(fig_location)
 
@@ -70,31 +62,26 @@ def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None,
 
 def plot_cluster(gdf: geopandas.GeoDataFrame, fig_location: str = None,
                  show_figure: bool = False):
-    """ Vykresleni grafu s lokalitou vsech
-        nehod v kraji shlukovanych do clusteru """
+    """ Vykresleni grafu s lokalitou vsech nehod v kraji shlukovanych do clusteru """
 
-    regions = ['PHA', 'STC', 'JHC', 'PLK', 'ULK', 'HKK',
-               'JHM', 'MSK', 'OLK', 'ZLK', 'VYS', 'PAK', 'LBK', 'KVK']
+    regions = ['PHA','STC','JHC','PLK','ULK','HKK','JHM','MSK','OLK','ZLK','VYS','PAK','LBK','KVK']
     gdf_c = gdf.copy()[gdf["region"] == "JHM"]
     gdf_c = gdf_c[gdf_c["p36"] == 1]
     gdf_c["area"] = gdf_c.area
     gdf_c = gdf_c.set_geometry(gdf_c.centroid)
     gdf_c = gdf_c.to_crs(epsg=3857)
 
-    coords = np.dstack([gdf_c.geometry.x, gdf_c.geometry.y]).reshape(-1, 2)
-    model = sklearn.cluster.MiniBatchKMeans(n_clusters=75)
+    coords = np.dstack([gdf_c.geometry.x, gdf_c.geometry.y]).reshape(-1,2)
+    model = sklearn.cluster.MiniBatchKMeans(n_clusters = 75)
     db = model.fit(coords)
     gdf4 = gdf_c.copy()
     gdf4["cluster"] = db.labels_
-    gdf4 = gdf4.dissolve(by="cluster", aggfunc={
-                         "p1": "count",
-                         "area": "sum"}).rename(columns={"p1": "cnt"})
+    gdf4 = gdf4.dissolve(by="cluster", aggfunc={"p1": "count", "area":"sum"}).rename(columns={"p1":"cnt"})
 
     plt.figure(figsize=(15, 8))
     ax = plt.gca()
     ax.axis("off")
-    ax.title.set_text("hustota nehod na cestach 1. triedy v JHM kraji")
-    gdf4.plot(ax=ax, column="cnt", legend=True, marker=".")
+    gdf4.plot(ax=ax, column="cnt", legend=True, marker=".") 
     ctx.add_basemap(ax, crs="epsg:3857", source=ctx.providers.Stamen.TonerLite)
 
     if fig_location is not None:
